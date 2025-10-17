7c657288df7f936ada8584c3185b8bf4d3c57af1
Saimor Raduan <saimorraduan@gmail.com>
2025-10-17 11:30:48 -0400

    Estudo sobre POO, projeto jogo COMPLETO

--- modulo_2/projeto/jogo.py
+++ modulo_2/projeto/jogo.py
@@ -1,3 +1,5 @@
+import random
+
 class Personagem:
     def __init__(self, nome, vida, nivel) -> None:
         self.__nome = nome
@@ -22,7 +24,7 @@ class Personagem:
             self.__vida = 0
 
     def atacar(self, alvo):
-        dano = self.__nivel * 2
+        dano = random.randint(self.get_nivel() * 2, self.get_nivel() * 4)
         alvo.receber_ataque(dano)
         print(f"{self.get_nome()} atacou {alvo.get_nome()} e causou {dano}")
         
@@ -36,6 +38,11 @@ class Heroi(Personagem):
     
     def exibir_detalhes(self):
         return f"{super().exibir_detalhes()}\nHabilidade: {self.get_habilidade()}"
+    
+    def ataque_especial(self, alvo):
+        dano = random.randint(self.get_nivel() * 5, self.get_nivel() * 8)
+        alvo.receber_ataque(dano)
+        print(f"{self.get_nome()} usou a habilidade especial {self.get_habilidade()} em {alvo.get_nome()} e causou {dano} de dano!")
     
 class Inimigo(Personagem):
     def __init__(self, nome, vida, nivel, tipo):
@@ -68,8 +75,13 @@ class Jogo:
 
             if escolha == '1':
                 self.heroi.atacar(self.inimigo)
+            elif escolha == '2':
+                self.heroi.ataque_especial(self.inimigo)
             else:
                 print("Escolha inválida, escolha novamente...")
+
+            if self.inimigo.get_vida() > 0:
+                self.inimigo.atacar(self.heroi)
 
         if self.heroi.get_vida() > 0:
             print("VITÓRIAAAAAAAA")
